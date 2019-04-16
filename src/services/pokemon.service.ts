e93c08c127aef68da32bb20adcb1a812ad8c0460
nyaRoot <root@whatever.com>
2019-04-16 16:04:31 +0200

    feat: add constants file

--- src/services/pokemon.service.ts
+++ src/services/pokemon.service.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
+import { WELCOME_MESSAGE } from "../constants/pokeApi.constants";
+
 export class PokeService {
   public welcomeMessage(req: Request, res: Response) {
-    return res.status(200).send("Welcome to pokeAPI REST");
+    return res.status(200).send(WELCOME_MESSAGE);
   }
 }
