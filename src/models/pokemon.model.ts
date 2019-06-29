fdfa34d94ee1bb991c05938704b3139454554b9d
nyaRoot <root@whatever.com>
2019-06-29 16:00:48 +0200

    feat(pokemon.model): remove mongoose _id property

--- src/models/pokemon.model.ts
+++ src/models/pokemon.model.ts
@@ -1,10 +1,20 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
-export const PokemonSchema = new mongoose.Schema({
-  Name: String,
-  Gender: String,
-  Type: String,
-  Height: Number,
-  Weight: Number,
-  Photo: String
+export const PokemonSchema = new mongoose.Schema(
+  {
+    name: String,
+    gender: String,
+    type: String,
+    height: Number,
+    weight: Number,
+    photo: String
+  },
+  { versionKey: false }
+);
+
+PokemonSchema.set("toJSON", {
+  transform: function(doc: any, ret: any, options: any) {
+    ret.id = ret._id;
+    delete ret._id;
+  }
 });
