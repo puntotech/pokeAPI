2c96fd676aefb8eae602c5a4866aab8d393da2d8
nyaRoot <root@whatever.com>
2019-07-04 12:21:26 +0200

    refactor(app.ts): add express typings

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
-import express from "express";
-import bodyParser from "body-parser";
-import cors from "cors";
-import { Controller } from "./main.controller";
-import mongoose from "mongoose";
+import { Application } from 'express';
+import { Controller } from './main.controller';
+import { PokeService } from './services/pokemon.service';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import express from 'express';
+import mongoose from 'mongoose';
 class App {
-  public app: any;
+  public app: Application;
   public pokeController: Controller;
 
   constructor() {
@@ -12,18 +14,18 @@ class App {
     this._setConfig();
     this._setMongoConfig();
 
-    this.pokeController = new Controller(this.app);
+    this.pokeController = new Controller(this.app, new PokeService());
   }
 
   private _setConfig() {
-    this.app.use(bodyParser.json({ limit: "50mb" }));
-    this.app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+    this.app.use(bodyParser.json({ limit: '50mb' }));
+    this.app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
     this.app.use(cors());
   }
 
   private _setMongoConfig() {
     mongoose.Promise = global.Promise;
-    mongoose.connect("mongodb://localhost:27017/Pokemon", {
+    mongoose.connect('mongodb://localhost:27017/Pokemon', {
       useNewUrlParser: true
     });
   }
