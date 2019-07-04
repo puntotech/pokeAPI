e4460ebcf2c24ee4302d655545a4133494d3f899
nyaRoot <root@whatever.com>
2019-07-04 13:07:08 +0200

    refactor(server.ts): remove fat arrow curly braces

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,4 @@
 import { PORT } from './constants/pokeApi.constants';
 import app from './app';
 
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
