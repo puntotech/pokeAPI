6b329f42689aabf88d1f979c35c30e96194002e9
nyaRoot <root@whatever.com>
2019-07-04 12:38:25 +0200

    refactor(main.controller): add express typings

--- src/main.controller.ts
+++ src/main.controller.ts
@@ -1,14 +1,15 @@
-import { PokeService } from "./services/pokemon.service";
+import { Application } from 'express';
+import { PokeService } from './services/pokemon.service';
 
 export class Controller {
   private pokeService: PokeService;
 
-  constructor(private app: any) {
+  constructor(private app: Application) {
     this.pokeService = new PokeService();
     this.routes();
   }
 
   public routes() {
-    this.app.route("/").get(this.pokeService.welcomeMessage);
+    this.app.route('/').get(this.pokeService.welcomeMessage);
   }
 }
