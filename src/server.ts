36670e123e393d467154afb3119bc59858b0c812
nyaRoot <root@whatever.com>
2019-07-04 13:00:52 +0200

    refactor(server.ts): add template literals with variable interpolation

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
-import app from "./app";
-import { PORT } from "./constants/pokeApi.constants";
+import { PORT } from './constants/pokeApi.constants';
+import app from './app';
 
 app.listen(PORT, () => {
-  console.log("Listening on port " + PORT);
+  console.log(`Listening on port ${PORT}`);
 });
