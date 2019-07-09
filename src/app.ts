7350a1e4492b9e062df7504b2607482e739fe983
nyaRoot <root@whatever.com>
2019-07-09 22:10:58 +0200

    refactor(app.ts): remove redundant underscore from private function names

--- src/app.ts
+++ src/app.ts
@@ -1,28 +1,30 @@
+import express, { Application } from 'express';
+
 import { Controller } from './main.controller';
 import { MONGO_URL } from './constants/pokeApi.constants';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import express, { Application } from 'express';
 import mongoose from 'mongoose';
+
 class App {
   public app: Application;
   public pokeController: Controller;
 
   constructor() {
     this.app = express();
-    this._setConfig();
-    this._setMongoConfig();
+    this.setConfig();
+    this.setMongoConfig();
 
     this.pokeController = new Controller(this.app);
   }
 
-  private _setConfig() {
+  private setConfig() {
     this.app.use(bodyParser.json({ limit: '50mb' }));
     this.app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
     this.app.use(cors());
   }
 
-  private _setMongoConfig() {
+  private setMongoConfig() {
     mongoose.Promise = global.Promise;
     mongoose.connect(MONGO_URL, {
       useNewUrlParser: true
