9cad127187df64abfab819fa623a4a11aead2a0a
Estefania Garcia Gallardo <nyablk97@gmail.com>
2021-02-11 16:17:50 +0100

    feat(app.ts): extract controller logic

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,26 @@
-import { Application } from 'express';
-import { Controller } from './main.controller';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import express from 'express';
+import { Application } from "express";
+import { Controller } from "./pokemon.controller";
+import bodyParser from "body-parser";
+import cors from "cors";
+import express from "express";
 class App {
   public app: Application;
-  public pokeController: Controller;
+  public pokemonController: Controller;
 
   constructor() {
     this.app = express();
     this.setConfig();
-
-    this.pokeController = new Controller(this.app);
+    this.setControllers();
   }
 
   private setConfig() {
-    this.app.use(bodyParser.json({ limit: '50mb' }));
-    this.app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+    this.app.use(bodyParser.json({ limit: "50mb" }));
+    this.app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
     this.app.use(cors());
+  }
+
+  private setControllers() {
+    this.pokemonController = new Controller(this.app);
   }
 }
 
