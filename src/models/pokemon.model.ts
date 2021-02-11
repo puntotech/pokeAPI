1a44da0e4aa01d32f65df0f0acce04cb3fa109a0
Estefania Garcia Gallardo <nyablk97@gmail.com>
2021-02-11 13:43:51 +0100

    feat(pokemon.model): add model type

--- src/models/pokemon.model.ts
+++ src/models/pokemon.model.ts
@@ -1,22 +1,25 @@
-import mongoose, { Schema } from 'mongoose';
+import { model, Schema } from "mongoose";
+import { IPokemon } from "../interfaces/pokemon.interface";
 
-export const PokemonSchema = new mongoose.Schema(
+export const PokemonSchema = new Schema(
   {
     name: String,
     gender: String,
     type: String,
     height: Number,
     weight: Number,
-    photo: String
+    photo: String,
   },
   { versionKey: false }
 );
 
-PokemonSchema.set('toJSON', {
-  transform: function(doc: any, ret: any, options: any) {
+PokemonSchema.set("toJSON", {
+  transform: function (doc: any, ret: any, options: any) {
     ret.id = ret._id;
     delete ret._id;
-  }
+  },
 });
 
-export const Pokemon = mongoose.model('Pokemon', PokemonSchema);
+const Pokemon = model<IPokemon>("Pokemon", PokemonSchema);
+
+export default Pokemon;
