c0577ebd315eaa31958991a0fc5ed7a71318635e
Carlos Caballero <carlos.caballero.gonzalez@gmail.com>
2021-02-14 12:37:56 +0100

    feat(constants): Create new constants

--- src/constants/pokeApi.constants.ts
+++ src/constants/pokeApi.constants.ts
@@ -1,3 +1,11 @@
 export const PORT = 9001;
 export const WELCOME_MESSAGE = "Welcome to pokeAPI REST by Nya ^^";
-export const MONGO_URL = "mongodb://localhost:27017/Pokemon";
+
+export const MONGO = {
+    url: 'mongodb://localhost:27017/Pokemon',
+    configuration: {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+    }
+}
