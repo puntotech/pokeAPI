a29ef8e7644a621437f549bf30b8f546dc856244
Estefania Garcia Gallardo <nyablk97@gmail.com>
2021-02-17 18:27:50 +0100

    feat(pokemon.model): type model, add required to schema

--- src/models/pokemon.model.ts
+++ src/models/pokemon.model.ts
@@ -1,12 +1,13 @@
-import mongoose from 'mongoose';
+import { IPokemon } from "../interfaces/pokemon.interface";
+import mongoose from "mongoose";
 
 const PokemonSchema = new mongoose.Schema({
-  name: String,
-  gender: String,
-  type: String,
-  height: Number,
-  weight: Number,
-  photo: String
+  name: { type: String, required: [true] },
+  gender: { type: String, required: [true] },
+  type: { type: String, required: [true] },
+  height: { type: Number, required: [true] },
+  weight: { type: Number, required: [true] },
+  photo: { type: String, required: [true] },
 });
 
-export const Pokemon = mongoose.model('Pokemon', PokemonSchema);
+export const Pokemon = mongoose.model<IPokemon>("Pokemon", PokemonSchema);
