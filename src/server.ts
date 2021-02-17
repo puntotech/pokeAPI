9e3b06cd0b4fbcd49e0c04a676f115bf8140d2f3
Estefania Garcia Gallardo <nyablk97@gmail.com>
2021-02-17 16:27:26 +0100

    refactor(server): update import

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import { PORT } from './constants/pokeApi.constants';
-import app from './app';
+import { PORT } from "./constants/pokeapi.constants";
+import app from "./app";
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
