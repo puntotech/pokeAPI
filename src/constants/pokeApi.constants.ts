84bf24b9348e0c9caba6fe9fa4e36caa28d40c4d
Estefania Garcia Gallardo <nyablk97@gmail.com>
2021-02-17 16:26:58 +0100

    refactor(pokeApi.constants): rename to pokeapi.constants

--- src/constants/pokeApi.constants.ts
+++
@@ -1,2 +0,0 @@
-export const PORT = 9001;
-export const WELCOME_MESSAGE = "Welcome to pokeAPI REST by Nya ^^";
---
+++ src/constants/pokeapi.constants.ts
@@ -0,0 +1,2 @@
+export const PORT = 9001;
+export const WELCOME_MESSAGE = "Welcome to PokeAPI REST by Nya ^^";
