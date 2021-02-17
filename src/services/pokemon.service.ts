9d2d6b53ccb3f9cace4b1f918d142c1e382a0ae1
Estefania Garcia Gallardo <nyablk97@gmail.com>
2021-02-17 18:26:32 +0100

    refactor(pokemon.service): remove coupled express logic

--- src/services/pokemon.service.ts
+++ src/services/pokemon.service.ts
@@ -1,9 +1,8 @@
-import { Request, Response } from 'express';
+import { Pokemon } from "../models/pokemon.model";
+import { WELCOME_MESSAGE } from "../constants/pokeApi.constants";
 
-import { WELCOME_MESSAGE } from '../constants/pokeApi.constants';
-
-export class PokeService {
-  public welcomeMessage(req: Request, res: Response) {
-    return res.status(200).send(WELCOME_MESSAGE);
+export class PokemonService {
+  public getWelcomeMessage(req: Request, res: Response) {
+    return WELCOME_MESSAGE;
   }
 }
