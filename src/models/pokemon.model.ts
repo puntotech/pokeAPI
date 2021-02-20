28bbf04c3d3a9e8199871647adad3e219377e22d
Estefania Garcia Gallardo <nyablk97@gmail.com>
2021-02-20 15:48:50 +0100

    feat(pokemon.model): add types, make fields required

--- src/models/pokemon.model.ts
+++ src/models/pokemon.model.ts
@@ -1,12 +1,13 @@
-import mongoose from 'mongoose';
+import { IPokemon } from "../interfaces/pokemon.interface";
+import mongoose from "mongoose";
 
 export const PokemonSchema = new mongoose.Schema({
-  name: String,
-  gender: String,
-  type: String,
-  height: Number,
-  weight: Number,
-  photo: String
+  name: { type: String, required: [true, "Field is required"] },
+  gender: { type: String, required: [true, "Field is required"] },
+  type: { type: String, required: [true, "Field is required"] },
+  height: { type: Number, required: [true, "Field is required"] },
+  weight: { type: Number, required: [true, "Field is required"] },
+  photo: { type: String, required: [true, "Field is required"] },
 });
 
-export const Pokemon = mongoose.model('Pokemon', PokemonSchema);
+export const Pokemon = mongoose.model<IPokemon>("Pokemon", PokemonSchema);
