b7cc38cec085282524466af7e47f3294b4c6481b
Estefania Garcia Gallardo <nyablk97@gmail.com>
2021-02-20 15:48:20 +0100

    refactor(app.ts): change controller instantiation logic

--- src/app.ts
+++ src/app.ts
@@ -1,34 +1,39 @@
-import express, { Application } from 'express';
-
-import { Controller } from './main.controller';
-import { MONGO_URL } from './constants/pokeApi.constants';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import mongoose from 'mongoose';
+import { MONGO_URL } from "./constants/pokeApi.constants";
+import { PokemonController } from "./pokemon.controller";
+import { PokemonService } from "./services/pokemon.service";
+import bodyParser from "body-parser";
+import cors from "cors";
+import express from "express";
+import mongoose from "mongoose";
 
 class App {
-  public app: Application;
-  public pokeController: Controller;
+  public app: express.Application;
 
   constructor() {
     this.app = express();
     this.setConfig();
     this.setMongoConfig();
-
-    this.pokeController = new Controller(this.app);
+    this.setControllers();
   }
 
   private setConfig() {
-    this.app.use(bodyParser.json({ limit: '50mb' }));
-    this.app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+    this.app.use(bodyParser.json({ limit: "50mb" }));
+    this.app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
     this.app.use(cors());
   }
 
   private setMongoConfig() {
     mongoose.Promise = global.Promise;
     mongoose.connect(MONGO_URL, {
-      useNewUrlParser: true
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
+  }
+
+  private setControllers() {
+    const pokemonController = new PokemonController(new PokemonService());
+
+    this.app.use("/pokemon", pokemonController.router);
   }
 }
 
