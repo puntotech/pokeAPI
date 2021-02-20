049802f532278e85bb281da54faa9a501ed2222f
Estefania Garcia Gallardo <nyablk97@gmail.com>
2021-02-20 15:59:07 +0100

    feat(pokemon.model): add custom error message to schema

--- src/models/pokemon.model.ts
+++ src/models/pokemon.model.ts
@@ -1,14 +1,15 @@
-import { model, Schema } from "mongoose";
+import { Schema, model } from "mongoose";
+
 import { IPokemon } from "../interfaces/pokemon.interface";
 
 export const PokemonSchema = new Schema(
   {
-    name: String,
-    gender: String,
-    type: String,
-    height: Number,
-    weight: Number,
-    photo: String,
+    name: { type: String, required: [true, "Field is required"] },
+    gender: { type: String, required: [true, "Field is required"] },
+    type: { type: String, required: [true, "Field is required"] },
+    height: { type: Number, required: [true, "Field is required"] },
+    weight: { type: Number, required: [true, "Field is required"] },
+    photo: { type: String, required: [true, "Field is required"] },
   },
   { versionKey: false }
 );
