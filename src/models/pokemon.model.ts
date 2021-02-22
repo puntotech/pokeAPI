1d0f9f98d5ef8b5d895a7b5c2364c75979ecc4fb
Estefania Garcia Gallardo <nyablk97@gmail.com>
2021-02-22 14:56:09 +0100

    refactor(pokemon.model): remove schema export

--- src/models/pokemon.model.ts
+++ src/models/pokemon.model.ts
@@ -2,7 +2,7 @@ import { Schema, model } from "mongoose";
 
 import { IPokemon } from "../interfaces/pokemon.interface";
 
-export const PokemonSchema = new Schema({
+const PokemonSchema = new Schema({
   name: { type: String, required: [true, "Field is required"] },
   gender: { type: String, required: [true, "Field is required"] },
   type: { type: String, required: [true, "Field is required"] },
