7c0756ad55ed27dc2a70adcdb00145b3641b5539
Estefania Garcia Gallardo <nyablk97@gmail.com>
2021-02-26 20:40:01 +0100

    feat(http.error): implement GenericError interface

--- src/errors/http.error.ts
+++ src/errors/http.error.ts
@@ -1,4 +1,6 @@
-export class HttpError extends Error {
+import { GenericError } from "../interfaces/generic-error.interface";
+
+export class HttpError extends Error implements GenericError {
   constructor(message: string, public status: number) {
     super(message);
   }
