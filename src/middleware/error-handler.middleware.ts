be68b8ecfdbce64109310d7264b96c17147f7d89
Estefania Garcia Gallardo <nyablk97@gmail.com>
2021-02-26 20:40:22 +0100

    refactor(error-handler.middleware): update error type

--- src/middleware/error-handler.middleware.ts
+++ src/middleware/error-handler.middleware.ts
@@ -1,6 +1,6 @@
-import { NextFunction, Request, Response } from "express";
+import { Request, Response } from "express";
 
-import { GenericError } from "../errors/generic-error.type";
+import { GenericError } from "../interfaces/generic-error.interface";
 
 export function handleErrors(err: GenericError, _: Request, res: Response) {
   const { status = 500, message } = err;
